b112205b1c25a34dff1219926cfd0cd2dd07ed50
Paule AD <akramaite.paule@gmail.com>
2017-04-02 18:59:30 +0100

    deal methods burger, eatery and day resturn objects

--- models/deal.rb
+++ models/deal.rb
@@ -1,3 +1,6 @@
+require_relative('burger')
+require_relative('eatery')
+
 class Deal
   attr_reader :name, :id
   attr_accessor :menu_item_id
@@ -32,23 +35,24 @@ class Deal
     sql = "SELECT burgers.* FROM burgers INNER JOIN menu_items
      ON menu_items.burger_id = burgers.id WHERE
      menu_items.id = #{@menu_item_id} "
-     result = SqlRunner.run(sql)
-     burger = Burger.new(burger)
+     result = SqlRunner.run(sql).first
+     burger = Burger.new(result)
      return burger
   end
 
   def eatery()
-    sql = "SELECT eateries.name FROM eateries INNER JOIN menu_items
+    sql = "SELECT eateries.* FROM eateries INNER JOIN menu_items
      ON menu_items.eatery_id = eateries.id WHERE
      menu_items.id = #{@menu_item_id} "
-     result = SqlRunner.run(sql)
-    eatery = result.first['name']
+     result = SqlRunner.run(sql).first
+    eatery = Eatery.new(result)
      return eatery
   end
 
   def day()
     sql = "SELECT days.name FROM days WHERE id = #{@day_id}"
-    result = SqlRunner.run(sql)
-    day = result.first['name']
+    result = SqlRunner.run(sql).first
+    day = Day.new(result)
+    return day
   end
 end

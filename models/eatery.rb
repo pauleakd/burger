368973a024b2fbf1b052654396c76f1fbaa6e992
Paule AD <akramaite.paule@gmail.com>
2017-04-03 12:12:08 +0100

    fixed eateries deals method

--- models/eatery.rb
+++ models/eatery.rb
@@ -22,10 +22,24 @@ class Eatery
   end
 
   def deals()
-    sql = "SELECT DISTINCT deals.* FROM deals INNER JOIN menu_items ON menu_items.id = deals.menu_item_id
+    sql = "SELECT DISTINCT deals_subtract.* FROM deals_subtract INNER JOIN menu_items
+     ON menu_items.id = deals_subtract.menu_item_id
     WHERE menu_items.eatery_id = #{@id}"
     results = SqlRunner.run(sql)
-    return results.map {|deal| Deal.new(deal)}
+    deals1 = results.map {|deal| DealSubtract.new(deal)}
+
+    sql = "SELECT DISTINCT deals_percent.* FROM deals_percent INNER JOIN menu_items ON menu_items.id = deals_percent.menu_item_id
+    WHERE menu_items.eatery_id = #{@id}"
+    results = SqlRunner.run(sql)
+    deals2 = results.map {|deal| DealPercent.new(deal)}
+
+    sql = "SELECT DISTINCT deals_x_for_y.* FROM deals_x_for_y
+     INNER JOIN menu_items ON menu_items.id = deals_x_for_y.menu_item_id
+    WHERE menu_items.eatery_id = #{@id}"
+    results = SqlRunner.run(sql)
+    deals3 = results.map {|deal| DealXforY.new(deal)}
+
+    return deals1 + deals2 + deals3
   end
 
   def self.all()

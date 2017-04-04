456560202c5b2c69e5064960407661d570008715
Paule AD <akramaite.paule@gmail.com>
2017-04-04 10:35:26 +0100

    can add burgers

--- controllers/burger_controller.rb
+++ controllers/burger_controller.rb
@@ -6,11 +6,19 @@ get "/burgers" do
   erb(:burgers)
 end
 
+
+get "/burgers/new" do
+  erb(:new_burger)
+end
+
+post "/burgers/new" do
+  @burger = Burger.new(params)
+  @burger.save()
+  redirect '/burgers'
+end
+
+
 get "/burgers/:id" do
   @burger = Burger.find(params[:id])
   erb(:show_burger)
-end
-
-get "burgers/new" do
-  erb(:new_burger)
 end
